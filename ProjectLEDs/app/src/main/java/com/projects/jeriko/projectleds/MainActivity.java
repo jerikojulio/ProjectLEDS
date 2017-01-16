44148467b42cf583bec1a54b53956cb09b73e0fd
WILLIAMROG\William M <william.m.skom@gmail.com>
2017-01-16 14:51:47 +0700

    //RED button now only set xValues yValues zValues, New method to do the parse the val to string and send it "parseit", Add Pink Color

--- ProjectLEDs/app/src/main/java/com/projects/jeriko/projectleds/MainActivity.java
+++ ProjectLEDs/app/src/main/java/com/projects/jeriko/projectleds/MainActivity.java
@@ -52,13 +52,19 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     public void redColor(View view){
+/*
         TextView textDebug = (TextView) findViewById(R.id.text01);
 
         String tempString;
         String tempX;
         resetDataInternal();
-
+*/
         xValue = 255;
+        yValue = 0;
+        zValue = 0;
+        parseit(xValue,yValue,zValue);
+
+/*  HANDLED BY NEW METHOD
         tempString = String.valueOf(xValue);
         tempX = "x" + tempString;
 
@@ -70,6 +76,7 @@ public class MainActivity extends AppCompatActivity {
             textDebug.setText("WebSocketConnect failed");
             e.printStackTrace();
         }
+ */
     }
 
 
@@ -132,4 +139,72 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+    public void pinkColor(View view)
+    {
+        xValue =255;
+        yValue = 99;
+        zValue = 99;
+        parseit(xValue,yValue,zValue);
+    }
+
+
+    public void parseit (int xValue,int  yValue, int zValue)
+    {
+        String tempString;
+        String tempX;
+        String tempY;
+        String tempZ;
+        TextView textDebug = (TextView) findViewById(R.id.text01);
+        TextView rDebug = (TextView) findViewById(R.id.text02);
+        TextView gDebug = (TextView) findViewById(R.id.text03);
+        TextView bDebug = (TextView) findViewById(R.id.text04);
+        tempString = String.valueOf(xValue);
+        tempX = "x" + tempString;
+        tempString = String.valueOf(yValue);
+        tempY = "y" + tempString;
+        tempString = String.valueOf(zValue);
+        tempZ = "z" + tempString;
+
+        try
+        {
+            WebSocketConnect ws = new WebSocketConnect();
+            ws.connect_to_server(tempX);
+            textDebug.setText("Success");
+            rDebug.setText(tempX);
+        }
+        catch (Exception e)
+        {
+            textDebug.setText("WebSocketConnect failed");
+            e.printStackTrace();
+            rDebug.setText("ERROR SENDING");
+        }
+        try
+        {
+            WebSocketConnect ws = new WebSocketConnect();
+            ws.connect_to_server(tempY);
+            textDebug.setText("Success");
+            gDebug.setText(tempY);
+        }
+        catch (Exception e)
+        {
+            textDebug.setText("WebSocketConnect failed");
+            e.printStackTrace();
+            gDebug.setText("ERROR SENDING");
+        }
+        try
+        {
+            WebSocketConnect ws = new WebSocketConnect();
+            ws.connect_to_server(tempZ);
+            textDebug.setText("Success");
+            bDebug.setText(tempZ);
+        }
+        catch (Exception e)
+        {
+            textDebug.setText("WebSocketConnect failed");
+            e.printStackTrace();
+            bDebug.setText("ERROR SENDING");
+        }
+
+    }
+
 }
