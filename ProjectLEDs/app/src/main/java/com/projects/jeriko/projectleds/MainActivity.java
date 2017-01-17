6337f2d6e87e859e8dd92526fe9ceb3deddf3e79
WILLIAMROG\William M <william.m.skom@gmail.com>
2017-01-17 23:48:58 +0700

    baten kolom ke3 ama ke4 belon; tombol plus dan minus ready; kode sekarang pake hexadecimal supaya sama dengan color picker; problem to solve : still brightness, kok tombol birunya berubah rubah yak?

--- ProjectLEDs/app/src/main/java/com/projects/jeriko/projectleds/MainActivity.java
+++ ProjectLEDs/app/src/main/java/com/projects/jeriko/projectleds/MainActivity.java
@@ -3,7 +3,6 @@ package com.projects.jeriko.projectleds;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -47,52 +46,128 @@ public class MainActivity extends AppCompatActivity {
 
         switch (view.getId()) {
             case R.id.red01:
-                xValue = 255;
+                xValue = 0xff;
                 yValue = 0;
                 zValue = 0;
                 parseit(xValue,yValue,zValue);
                 break;
 
             case R.id.red02:
-                xValue = 253;
-                yValue = 42;
+                xValue = 0xfd;
+                yValue = 0x2a;
                 zValue = 0;
                 parseit(xValue,yValue,zValue);
                 break;
 
             case R.id.red03:
-                xValue = 255;
-                yValue = 68;
+                xValue = 0xff;
+                yValue = 0x44;
                 zValue = 0;
                 parseit(xValue,yValue,zValue);
                 break;
             case R.id.red04:
-                xValue = 255;
-                yValue = 157;
+                xValue = 0xff;
+                yValue = 0x9d;
                 zValue = 0;
                 parseit(xValue,yValue,zValue);
                 break;
 
             case R.id.red05:
-                xValue = 251;
-                yValue = 255;
+                xValue = 0xfb;
+                yValue = 0xff;
                 zValue = 0;
                 parseit(xValue,yValue,zValue);
                 break;
 
             case R.id.green01:
                 xValue = 0;
-                yValue = 148;
+                yValue = 0xb0;
                 zValue = 0;
                 parseit(xValue,yValue,zValue);
                 break;
 
             case R.id.green02:
-                xValue = 12;
-                yValue = 200;
-                zValue = 12;
+                xValue = 0x0c;
+                yValue = 0xc8;
+                zValue = 0x0c;
                 parseit(xValue,yValue,zValue);
                 break;
+
+            case R.id.green03:
+                xValue = 0x00;
+                yValue = 0x90;
+                zValue = 0x90;
+                parseit(xValue,yValue,zValue);
+                break;
+
+            case R.id.green04:
+                xValue = 0x00;
+                yValue = 0x80;
+                zValue = 0x00;
+                parseit(xValue,yValue,zValue);
+                break;
+
+            case R.id.green05:
+                xValue = 0x00;
+                yValue = 0x30;
+                zValue = 0x00;
+                parseit(xValue,yValue,zValue);
+                break;
+
+            case R.id.rplus:
+                xValue = xValue+8;
+                if (xValue>=0xff)
+                {
+                    xValue=0xff;
+                }
+                parseit(xValue,yValue,zValue);
+                break;
+
+            case R.id.gplus:
+                yValue = yValue+8;
+                if (yValue>=0xff)
+                {
+                    yValue=0xff;
+                }
+                parseit(xValue,yValue,zValue);
+                break;
+
+            case R.id.bplus:
+                zValue = zValue+8;
+                if (zValue>=0xff)
+                {
+                    zValue=0xff;
+                }
+                parseit(xValue,yValue,zValue);
+                break;
+
+            case R.id.rminus:
+                xValue = xValue-8;
+                if (xValue<=0x00)
+                {
+                    xValue=0x00;
+                }
+                parseit(xValue,yValue,zValue);
+                break;
+
+            case R.id.gminus:
+                yValue = yValue-8;
+                if (yValue<=0x00)
+                {
+                    yValue=0x00;
+                }
+                parseit(xValue,yValue,zValue);
+                break;
+
+            case R.id.bminus:
+                zValue = zValue-8;
+                if (zValue<=0x0)
+                {
+                    zValue=0x00;
+                }
+                parseit(xValue,yValue,zValue);
+                break;
+
         }
     }
 
